d6a67777d9c5c767ce579b0d5655ccfca8e765d5
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-08-31 00:04:37 -0500

    [add] agregando soporte para operaciones aritmeticas mas complejas

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,41 +1,118 @@
+// src/Calculator.java
+
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Calculator {
     public double evaluate(String input) throws Exception {
-        String[] tokens;
-        double num1, num2;
+        Queue<String> postfix = infixToPostfix(input);
+        return evaluatePostfix(postfix);
+    }
 
-        if (input.contains("+")) {
-            tokens = input.split("\\+");
-            num1 = Double.parseDouble(tokens[0]);
-            num2 = Double.parseDouble(tokens[1]);
-            return num1 + num2;
-        } else if (input.contains("-")) {
-            tokens = input.split("-");
-            num1 = Double.parseDouble(tokens[0]);
-            num2 = Double.parseDouble(tokens[1]);
-            return num1 - num2;
-        } else if (input.contains("*")) {
-            tokens = input.split("\\*");
-            num1 = Double.parseDouble(tokens[0]);
-            num2 = Double.parseDouble(tokens[1]);
-            return num1 * num2;
-        } else if (input.contains("/")) {
-            tokens = input.split("/");
-            num1 = Double.parseDouble(tokens[0]);
-            num2 = Double.parseDouble(tokens[1]);
-            if (num2 == 0) {
-                throw new Exception("Error: División por cero.");
-            }
-            return num1 / num2;
-        } else if (input.contains("^")) {
-            tokens = input.split("\\^");
-            num1 = Double.parseDouble(tokens[0]);
-            num2 = Double.parseDouble(tokens[1]);
-            return Math.pow(num1, num2);
-        } else if (input.startsWith("sqrt")) {
-            num1 = Double.parseDouble(input.substring(4));
-            return Math.sqrt(num1);
-        } else {
-            throw new Exception("Operación no reconocida.");
+    private int getPrecedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
         }
+        return -1;
+    }
+
+    private Queue<String> infixToPostfix(String exp) {
+        Stack<Character> stack = new Stack<>();
+        Queue<String> result = new LinkedList<>();
+        String number = "";
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+
+            if (Character.isDigit(c) || c == '.') {
+                number += c;
+            } else {
+                if (!number.isEmpty()) {
+                    result.add(number);
+                    number = "";
+                }
+
+                switch (c) {
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '^':
+                        while (!stack.isEmpty() && getPrecedence(c) <= getPrecedence(stack.peek())) {
+                            result.add(String.valueOf(stack.pop()));
+                        }
+                        stack.push(c);
+                        break;
+                    case '(':
+                        stack.push(c);
+                        break;
+                    case ')':
+                        while (!stack.isEmpty() && stack.peek() != '(') {
+                            result.add(String.valueOf(stack.pop()));
+                        }
+                        if (!stack.isEmpty() && stack.peek() != '(') {
+                            return null; // Invalid expression
+                        } else {
+                            stack.pop();
+                        }
+                        break;
+                }
+            }
+        }
+
+        if (!number.isEmpty()) {
+            result.add(number);
+        }
+
+        while (!stack.isEmpty()) {
+            result.add(String.valueOf(stack.pop()));
+        }
+
+        return result;
+    }
+
+    private double evaluatePostfix(Queue<String> postfix) throws Exception {
+        Stack<Double> stack = new Stack<>();
+
+        while (!postfix.isEmpty()) {
+            String token = postfix.poll();
+
+            if (Character.isDigit(token.charAt(0))) {
+                stack.push(Double.parseDouble(token));
+            } else {
+                double num2 = stack.pop();
+                double num1 = stack.pop();
+
+                switch (token.charAt(0)) {
+                    case '+':
+                        stack.push(num1 + num2);
+                        break;
+                    case '-':
+                        stack.push(num1 - num2);
+                        break;
+                    case '*':
+                        stack.push(num1 * num2);
+                        break;
+                    case '/':
+                        if (num2 == 0) {
+                            throw new Exception("Error: División por cero.");
+                        }
+                        stack.push(num1 / num2);
+                        break;
+                    case '^':
+                        stack.push(Math.pow(num1, num2));
+                        break;
+                }
+            }
+        }
+        return stack.pop();
     }
 }
